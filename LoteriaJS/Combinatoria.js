845c20d1c061f6476a94884b10056e2d8a91e8ca
Richard Natal <bigous@gmail.com>
2015-08-26 23:22:14 -0300

    Código de combinatória está com variáveis com nomes mais legíveis (a pedido de carcamano) e aproveitei para comentar a rotina.

--- LoteriaJS/Combinatoria.js
+++ LoteriaJS/Combinatoria.js
@@ -1,46 +1,49 @@
 ﻿'use strict';
 
 function combinatoria(from, choose, callback) {
-    var i, j = 1, x;
-    var c = new Array(choose + 3);
+    var i, j = 1; //< Variaveis i e j são iteradores para acesso a itens da matriz
+    var tmp; //< Variável para substituição de um dos valores da combinação atual para o próximo valor da permutação
+    var combinacao = new Array(choose + 3); //< Combinação atual com 3 variaveis a mais para controle de execução
     
-    // inicia combinacao
+    // inicia combinacao atual com os valores iniciais de permutação (1,2,3,4,5,6 para amega sena por exemplo)
     for (i = 1; i <= choose; i++) {
-        c[i] = i;
+        combinacao[i] = i;
     }
-    c[choose + 1] = from + 1;
-    c[choose + 2] = 0;
+    combinacao[choose + 1] = from + 1;
+    combinacao[choose + 2] = 0;
     j = choose;
     
     for (;;) {
-        callback(c, choose);
+        callback(combinacao, choose);
         
         if (j > 0) {
-            x = j + 1;
+            // troca o número da combinação de traz para frente adicionando 1 ao valor atual
+            tmp = j + 1;
         } else {
-            
-            if (c[1] + 1 < c[2]) {
-                c[1] += 1;
+            // Quando chega ao primeiro valor, altera ele até que ele "cole" no segundo ou reseta o J para a próxima posição a ser alterada (que pode ser a última da combinação)
+            if (combinacao[1] + 1 < combinacao[2]) {
+                combinacao[1] += 1;
                 continue;
             }
             
             j = 2;
             
             for (;;) {
-                c[j - 1] = j - 1;
-                x = c[j] + 1;
-                if (x == c[j + 1]) {
+                combinacao[j - 1] = j - 1;
+                tmp = combinacao[j] + 1;
+                if (tmp == combinacao[j + 1]) {
                     j++;
                 } else {
                     break;
                 }
             }
             
+            // caso o próximo indice a ser alterado ultrapace a quantidade de itens a escolher, acabaram-se as permutações possíveis.
             if (j > choose)
                 break;
 
         }
-        c[j] = x;
+        combinacao[j] = tmp;
         j--;
     }
 }
